34ba41a6d0b83b9cc5bfdf77d79a8976e1eab61f
Kaishuto <32487322+Kaishuto@users.noreply.github.com>
2017-10-03 21:41:03 +0700

    Update botkick.py

--- botkick.py
+++ botkick.py
@@ -126,8 +126,8 @@ def SEND_MESSAGE(op):
             if msg.contentType == 0:
                 #if "gname:" in msg.text:
 if msg.text == "set":
-  sendMessage(msg.to, "udah saya set gan \n「tes」nih list yg read")
-             try:
+                 sendMessage(msg.to, "udah saya set gan \n「tes」nih list yg read")
+                    try:
                         del wait['readPoint'][msg.to]
                         del wait['readMember'][msg.to]
                     except:
